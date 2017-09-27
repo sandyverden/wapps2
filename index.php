b1a375f4b4c4f82383977514fadd33e948dcd581
sandy budiman <sandyverden@gmail.com>
2017-09-27 12:36:56 +0700

    test2

--- index.php
+++ index.php
@@ -4,4 +4,24 @@
 	echo "developer lainnya";
 	echo "<p>";
 	echo "developer lainnya2";
+	echo "Hello ini autodeploy harusnya :D tambahan tambahan2 test2 tamabahan 4 autodeploy";
+	echo "<p>";
+	echo "developer lainnya";
+	echo "<p>";
+	echo "developer lainnya2";
+	echo "Hello ini autodeploy harusnya :D tambahan tambahan2 test2 tamabahan 4 autodeploy";
+	echo "<p>";
+	echo "developer lainnya";
+	echo "<p>";
+	echo "developer lainnya2";
+	echo "Hello ini autodeploy harusnya :D tambahan tambahan2 test2 tamabahan 4 autodeploy";
+	echo "<p>";
+	echo "developer lainnya";
+	echo "<p>";
+	echo "developer lainnya2";
+	echo "Hello ini autodeploy harusnya :D tambahan tambahan2 test2 tamabahan 4 autodeploy";
+	echo "<p>";
+	echo "developer lainnya";
+	echo "<p>";
+	echo "developer lainnya2";
 ?>
